6780e02157b4fb706d91d65da3fedb35a74ebc74
claywasher <46997038+claywasher@users.noreply.github.com>
2019-04-18 13:10:15 +0700

    Update and rename Sqlmap.sh to smsapi.py

---
+++ smsapi.py
@@ -0,0 +1,11 @@
+Tools sms api ini coK
+print "helloo"
+print "aku mr.calamity"
+print "tools sms api"
+   print"masukkan no target="
+   print"masukkan nama pengirim="
+   print"pesan="
+   termux-sms-send -n 087700935379 heloo kontol
+
+ print"compelete"
+ print"goodbye see you or next tools"
